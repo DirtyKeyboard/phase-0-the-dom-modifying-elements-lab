d0077a32e4ad860ff350f7d26a2a8c5d64adc2c8
Andrew Hawileh <andrewh829@gmail.com>
2023-02-22 16:13:52 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+const main = document.querySelector("main#main");
+main.remove();
+const newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.textContent = "ANDREW is the champion";
+
+console.log(newHeader)
